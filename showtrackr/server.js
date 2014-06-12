f953f00e6f7acb99e57e38a750f681d9d5f455a4
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-11 21:19:50 -0400

    Adding route for getting shows

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -78,6 +78,22 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/api/shows', function(req, res, next) {
+  var query = Show.find();
+  if (req.query.genre) {
+    query.where({ genre: req.query.genre });
+  } else if (req.query.alphabet) {
+    query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') });
+  } else {
+    query.limit(12);
+  }
+  query.exec(function(err, shows) {
+    if (err) return next(err);
+    res.send(shows);
+  });
+});
+
+
 app.listen(app.get('port'), function(){
     console.log('Express server listening on port: ' + app.get('port'));
 });
