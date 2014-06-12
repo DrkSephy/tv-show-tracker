f4393cca747c2d7b8e5ae568cb3d41f90753da21
David Leonard <sephirothcloud1025@yahoo.com>
2014-06-11 21:08:58 -0400

    Adding password generation and validation methods

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -32,6 +32,33 @@ var showSchema = new mongoose.Scheme({
     }]
 });
 
+var userSchema = new mongoose.Schema({
+    email: { type: String, unique: true },
+    password: String
+});
+
+// Password generation
+userSchema.pre('save', function(next){
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function(err, salt){
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+// Password validation
+userSchema.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -42,5 +69,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function(){
-    console.log('Express server listening on port' + app.get('port'));
+    console.log('Express server listening on port: ' + app.get('port'));
 });
